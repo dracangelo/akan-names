aad3231f92acaa37bfbacb91621e4a85c984f5f2
dracangelo <kelvinmbuguaw@gmail.com>
2019-10-25 15:51:26 +0300

    added two variables

--- js/script.js
+++ js/script.js
@@ -1,2 +1,4 @@
 alert ("welcome!!!");
 
+var daysOfTheWeek =["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var maleName = ["Kwasi", "Kwado", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
