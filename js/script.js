7d4e8a2aee6cec6a7f2bb8df8648a6e64f2a088b
dracangelo <kelvinmbuguaw@gmail.com>
2019-10-25 16:35:26 +0300

    intoduced an if

--- js/script.js
+++ js/script.js
@@ -2,3 +2,35 @@ alert ("welcome!!!");
 
 var daysOfTheWeek =["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 var maleName = ["Kwasi", "Kwado", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+var femaleName = ["Akosua", "Adwoa", "Abenna", "Aku", "Yaa", "Afua", "Ama"];
+
+var cc = parseInt(document.getElementById("century").value);
+var yy = parseInt(document.getElementById("year").value);
+var mm = parseInt(document.getElementById("month").value);
+var dd = parseInt(document.getElementById("day").value);
+var result =
+ parseInt(
+   century / 4 -
+     2 * century -
+     1 +
+     (5 * year) / 4 +
+     (26 * (month + 1)) / 10 +
+     day
+ ) % 7;
+ if (document.getElementById("gender").checked) {
+   var gender = 'male';
+ }
+ else {
+   var gender = 'female';
+ }
+
+}
+if(Math.round(result)=== 0 && gender ==="male") {
+    doucument.getElementById("output").innerHTML = ("You were born on" + dayOfTheWeek[0]+ " and your Akan Name is" + maleNames[0])
+}
+else if(Math.round(result)=== 0 && gender ==="female") {
+  doucument.getElementById("output").innerHTML = ("You were born on" + dayOfTheWeek[0]+ " and your Akan Name is" + femaleNames[0])
+}
+else if(Math.round(result)=== 1 && gender ==="male") {
+    doucument.getElementById("output").innerHTML = ("You were born on" + dayOfTheWeek[1]+ " and your Akan Name is" + maleNames[1])
+  }
