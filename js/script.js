0d18cb6cf8ea554585a91959282ac8d44ae43309
dracangelo <kelvinmbuguaw@gmail.com>
2019-10-28 09:31:00 +0300

    modified the form

--- js/script.js
+++ js/script.js
@@ -1,44 +1,30 @@
 var submit = function() {
-    var days = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday"
-    ];
-    var male = [
-      "Kwasi",
-      "Kwadwo",
-      "Kwabena",
-      "Kwaku",
-      "Yaw",
-      "Kofi",
-      "Kwame"
-    ];
-    var female = [
-      "Akosua",
-      " Adwoa",
-      "Abenaa",
-      "Akua",
-      "Yaa",
-      "Afua",
-      "Ama"
-    ];
-    var month = parseInt(document.getElementById("month").value);
-    var year = parseInt(document.getElementById("year").value);
-    var day = parseInt(document.getElementById("day").value);
-    var date0fbirth = new Date(year + "/" + month + "/" + day);
-    var results = date0fbirth.getDay();
-    var done= document.getElementById("done");
-    var male = document.getElementById("male")
-    var female = document.getElementById("female")
-    if (male.checked==true) {
-      switch (days) {
-          case 0:
-              done.innnerHtml=("Your akan name is: " + male[days]);
-          case 1:
+  var days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+  var male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+  var female = ["Akosua", " Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+  var month = parseInt(document.getElementById("month").value);
+  var year = parseInt(document.getElementById("year").value);
+  var day = parseInt(document.getElementById("day").value);
+  var date0fbirth = new Date(year + "/" + month + "/" + day);
+  var results = date0fbirth.getDay();
+  var done = document.getElementById("done");
+  var male = document.getElementById("male");
+  var female = document.getElementById("female");
+  if (male.checked) {
+    return (done.innnerHtml =
+      "Your akan name is: " +
+      male[results] +
+      "and you were born on " +
+      days[results]);
+    /*case 1:
             done.innnerHtml=("your akan name is: " + male[days]);
           case 2:
             done.innnerHtml=("Your akan name is: " + male[days]);
@@ -49,11 +35,9 @@ var submit = function() {
           case 5:
             done.innnerHtml=("Your akan name is: " + male[days]);
           case 6:
-            done.innnerHtml=("Your akan name is: " + male[days]);
-      }
-  }
-  else if (female.checked==true)
-      switch (days) {
+            done.innnerHtml=("Your akan name is: " + male[days]);*/
+  } else if (female.checked) {
+    /*switch (days) {
           case 0:
             done.innnerHtml=("your akan name is: " + female[days]);
           case 1:
@@ -68,6 +52,11 @@ var submit = function() {
             done.innnerHtml=("your akan name is: " + female[days]);
           case 6:
             done.innnerHtml=("your akan name is: " + female[days]);
-      }
-
-    };
+      }*/
+    done.innnerHtml =
+      "Your akan name is: " +
+      female[results] +
+      "and you were born on " +
+      days[results];
+  }
+};
